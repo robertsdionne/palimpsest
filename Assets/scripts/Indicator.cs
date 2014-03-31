b6c82ab967d8c820e05b6ec461dff1272b1e4beb
Robert Dionne <robertsdionne@gmail.com>
2014-03-30 23:50:43 -0400

    Add correct directionality, distance, and containment for display of arrows and listings.

--- Assets/scripts/Indicator.cs
+++ Assets/scripts/Indicator.cs
@@ -9,9 +9,37 @@ public class Indicator : MonoBehaviour {
   public GameObject target;
 	
 	void FixedUpdate () {
-    var delta = target.transform.position - player.transform.position;
+    var delta = DirectionFrom(target, player.transform.position);
+    var distance = DistanceTo(target, player.transform.position);
     var angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
     var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     arrow.transform.rotation = Quaternion.Slerp(arrow.transform.rotation, rotation, 0.1f);
+    var scale = 0.125f * System.Convert.ToSingle(distance > 0.0f);
+    arrow.transform.localScale = Vector2.Lerp(
+        arrow.transform.localScale, new Vector2(scale, scale), 0.1f);
 	}
+
+  Vector2 DirectionFrom(GameObject target, Vector2 position) {
+    var dx = new Vector2(1e-5f, 0.0f);
+    var dy = new Vector2(0.0f, 1e-5f);
+    var direction = -new Vector2(
+        DistanceTo(target, position + dx) - DistanceTo(target, position - dx),
+        DistanceTo(target, position + dy) - DistanceTo(target, position - dy));
+    return direction.magnitude > 0.0f ? direction.normalized : new Vector2();
+  }
+
+  float DistanceTo(GameObject target, Vector2 position) {
+    Vector2 center = target.transform.position;
+    if (target.GetComponent<BoxCollider2D>().enabled) {
+      Vector2 halfExtent = target.transform.localScale / 2.0f;
+      return (Vector2.Max(Abs(center - position) - halfExtent, new Vector2())).magnitude;
+    } else {
+      float radius = target.transform.localScale.x;
+      return (center - position).magnitude - radius;
+    }
+  }
+
+  Vector2 Abs(Vector2 input) {
+    return new Vector2(Mathf.Abs(input.x), Mathf.Abs(input.y));
+  }
 }
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -31,11 +31,26 @@ public class Player : MonoBehaviour {
     MaybeSee();
   }
 
+  Vector2 Abs(Vector2 input) {
+    return new Vector2(Mathf.Abs(input.x), Mathf.Abs(input.y));
+  }
+
   void Describe(GameObject item) {
     if (item.GetComponent<Collider2D>().isTrigger) {
       item.GetComponent<Area>().Describe();
     } else {
       item.GetComponent<Scenery>().Describe();
+    }
+  }
+
+  float DistanceTo(GameObject target, Vector2 position) {
+    Vector2 center = target.transform.position;
+    if (target.GetComponent<BoxCollider2D>().enabled) {
+      Vector2 halfExtent = target.transform.localScale / 2.0f;
+      return (Vector2.Max(Abs(center - position) - halfExtent, new Vector2())).magnitude;
+    } else {
+      float radius = target.transform.localScale.x;
+      return (center - position).magnitude - radius;
     }
   }
 
@@ -51,14 +66,14 @@ public class Player : MonoBehaviour {
     if (Input.GetButtonDown(SEE)) {
       var items = GameObject.FindGameObjectsWithTag(SCENERY);
       var areas = (GameObject.FindObjectsOfType(typeof(Area)) as Area[]).Where(
-          area => area.IsOccupied()).ToList();
-      var nearestItems = items.OrderBy(item =>
-          Vector2.Distance(item.transform.position, gameObject.transform.position)).ToList();
+          area => area.IsOccupied()).Select(area => area.gameObject).ToList();
+      var nearestItems = items.Where(item => !areas.Contains(item)).OrderBy(item =>
+          DistanceTo(item, gameObject.transform.position)).ToList();
       Debug.Log("");
       WebsocketServer.BroadcastText("");
       TextConsole.PushText("");
       for (var i = 0; i < areas.Count; ++i) {
-        areas[i].Inside();
+        areas[i].GetComponent<Area>().Inside();
       }
       for (var i = 0; i < 3; ++i) {
         Describe(nearestItems[i]);
