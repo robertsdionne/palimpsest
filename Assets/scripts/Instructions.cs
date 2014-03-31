bd5fa605a4730e5403c534dff0ec23a85ca650e8
Robert Dionne <robertsdionne@gmail.com>
2014-03-31 18:48:31 -0400

    Setup instructions.

--- Assets/scripts/Instructions.cs
+++ Assets/scripts/Instructions.cs
@@ -15,12 +15,18 @@ public class Instructions : MonoBehaviour {
     if (showGui) {
       var style = new GUIStyle();
       style.font = fontEva;
-      GUI.Label(new Rect(100, 100, 100, 20), "Palimpsest", style);
+      GUI.Label(new Rect(80, 40, 100, 20), "Palimpsest", style);
       style.font = fontUbuntu;
-      GUI.Label(new Rect(100, 200, 100, 20), "Palimpsest", style);
-      GUI.Label(new Rect(100, 300, ps4LeftStick.width, ps4LeftStick.height), ps4LeftStick);
-      GUI.Label(new Rect(100, 400, ps4Cross.width, ps4Cross.height), ps4Cross);
-      GUI.Label(new Rect(100, 500, ps4L1.width, ps4L1.height), ps4L1);
+      GUI.Label(new Rect(80, 90, 100, 20), "Palimpsest", style);
+
+      GUI.Label(new Rect(80, 300, ps4LeftStick.width, ps4LeftStick.height), ps4LeftStick);
+      GUI.Label(new Rect(180, 350, 100, 20), "Walk.", style);
+
+      GUI.Label(new Rect(330, 300, ps4L1.width, ps4L1.height), ps4L1);
+      GUI.Label(new Rect(430, 350, 100, 20), "Run.", style);
+
+      GUI.Label(new Rect(580, 300, ps4Cross.width, ps4Cross.height), ps4Cross);
+      GUI.Label(new Rect(680, 350, 100, 20), "See.", style);
     }
 	}
 }
