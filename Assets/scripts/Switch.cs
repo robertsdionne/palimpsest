f389e7ddc87f32b3bc5747eba4779450a7d92c4c
Robert Dionne <robertsdionne@gmail.com>
2014-04-03 16:43:54 -0400

    Add doors and a building.

---
+++ Assets/scripts/Switch.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : Area {
+
+  public Obstacle door;
+  public Area doorway;
+
+  private bool state = false;
+
+  void OnTriggerEnter2D() {
+    occupied = true;
+    seen = true;
+    state = !state;
+    if (null != doorway) {
+      doorway.gameObject.SetActive(state);
+    }
+    if (null != door) {
+      door.gameObject.SetActive(!state);
+    }
+    if (enter.Length > 0) {
+      TextConsole.PushText(Choose(enter));
+    }
+  }
+
+  void OnTriggerExit2D() {
+    occupied = false;
+    seen = true;
+    if (exit.Length > 0) {
+      TextConsole.PushText(Choose(exit));
+    }
+  }
+}
