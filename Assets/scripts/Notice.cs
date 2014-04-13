bbac65c7ffc82166bdfdaeab37c800f9aab3c58d
Robert Dionne <robertsdionne@gmail.com>
2014-04-13 17:44:08 -0400

    Add alerts and notices. Fix path icons.

---
+++ Assets/scripts/Notice.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Notice : MonoBehaviour {
+
+  public string[] notices;
+
+  void OnTriggerEnter2D(Collider2D other) {
+    foreach (var notice in notices) {
+      TextConsole.PushText(notice);
+    }
+    Destroy(gameObject);
+  }
+}
