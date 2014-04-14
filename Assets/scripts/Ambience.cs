ed0d3052365a8f803f425f578aa9b044af711c79
Robert Dionne <robertsdionne@gmail.com>
2014-04-13 21:50:16 -0400

    Git is stupid.

---
+++ Assets/scripts/Ambience.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ambience : MonoBehaviour {
+
+  public string[] alerts;
+  public float period;
+
+  void OnTriggerStay2D(Collider2D other) {
+    if (Random.value < 1.0f / 60.0f / period) {
+      TextConsole.PushText(Choose(alerts));
+    }
+  }
+
+  protected string Choose(string[] choices) {
+    return choices.Length > 0 ? choices[Random.Range(0, choices.Length)] : null;
+  }
+}
