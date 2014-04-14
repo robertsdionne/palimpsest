5660cb552293a40ed862b65df90f3fe6a297dcc1
Robert Dionne <robertsdionne@gmail.com>
2014-04-13 21:23:39 -0400

    Add some storytelling potential to Notice.

--- Assets/scripts/Notice.cs
+++ Assets/scripts/Notice.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class Notice : MonoBehaviour {
 
   public string[] notices;
+  public GameObject[] nexts;
 
   void OnTriggerEnter2D(Collider2D other) {
     foreach (var notice in notices) {
       TextConsole.PushText(notice);
+    }
+    foreach (var next in nexts) {
+      next.SetActive(true);
     }
     Destroy(gameObject);
   }
