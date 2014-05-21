c9306ab1d34dcd5c2058f2506fbc71b74ec59861
Robert Dionne <robertsdionne@gmail.com>
2014-05-21 14:13:01 -0400

    Hint at desired frame rate.

--- Assets/scripts/CameraDebug.cs
+++ Assets/scripts/CameraDebug.cs
@@ -7,6 +7,11 @@ public class CameraDebug : MonoBehaviour {
   public const int PLAYER_AND_TEXT_MASK = 256 | 512;
 
   public bool debug = false;
+  public int targetFrameRate = 60;
+
+  void Start() {
+    Application.targetFrameRate = targetFrameRate;
+  }
 
   void Update () {
     if (debug) {
