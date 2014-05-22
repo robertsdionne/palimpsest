0a9541bf8d0487f44d62f501cba5d1a60b1531f2
Robert Dionne <robertsdionne@gmail.com>
2014-05-22 15:47:33 -0400

    Add restart code and controls.

---
+++ Assets/scripts/RestartTimeout.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartTimeout : MonoBehaviour {
+
+  public float timeout = 90.0f;
+
+  private static RestartTimeout instance = null;
+
+  private float lastInteractTime = 0.0f;
+
+  void Start() {
+    instance = this;
+    Interact();
+  }
+
+  public static void Interact() {
+    if (null != instance) {
+      instance.lastInteractTime = Time.time;
+    }
+  }
+
+	void Update() {
+    if (Input.GetButtonDown("Restart") || Time.time - lastInteractTime > timeout) {
+      Application.LoadLevel("title");
+    }
+	}
+}
